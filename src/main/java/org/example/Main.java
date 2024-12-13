6784ca541411fcddccf8b8f1296494d1881dd8dc
PatricioPonce28 <geanotoponce95@gmail.com>
2024-12-13 07:26:55 -0500

    Commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        JFrame frame = new JFrame("Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new FlowLayout());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        JLabel lblUsuario = new JLabel("Usuario:");
+        JTextField txtUsuario = new JTextField(15);
+
+        JLabel lblContrasena = new JLabel("Contraseña:");
+        JPasswordField txtContrasena = new JPasswordField(15);
+
+        JButton btnLogin = new JButton("Login");
+
+        JLabel lblMensaje = new JLabel("");
+
+        frame.add(lblUsuario);
+        frame.add(txtUsuario);
+        frame.add(lblContrasena);
+        frame.add(txtContrasena);
+        frame.add(btnLogin);
+        frame.add(lblMensaje);
+
+        btnLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String usuario = txtUsuario.getText();
+                String contrasena = new String(txtContrasena.getPassword());
+
+                if ("PATRICIO".equals(usuario) && "PATRICIO123".equals(contrasena)) {
+                    lblMensaje.setText("Acceso correcto");
+                    lblMensaje.setForeground(Color.GREEN);
+                } else {
+                    lblMensaje.setText("Usuario o contraseña incorrectos");
+                    lblMensaje.setForeground(Color.RED);
+                }
+            }
+        });
+
+        frame.setVisible(true);
     }
 }
